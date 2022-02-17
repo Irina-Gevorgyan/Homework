432f78f81eaa41534872ac57a87509b72d0f736e
Irina-Gevorgyan <99883345+Irina-Gevorgyan@users.noreply.github.com>
2022-02-17 19:57:27 +0400

    Update Tasks.py

--- Tasks.py
+++ Tasks.py
@@ -1,5 +1,5 @@
 #1
-def Payment():
+def payment():
     working_hours = float(input("Enter hours: "))
     rate_of_wage = float(input("Enter rate: "))
     pay = working_hours * rate_of_wage
@@ -7,11 +7,11 @@ def Payment():
 
 
 #2
-def Numbers():
+def numbers():
     sum = count = 0
-    while (True):
+    while True:
         number = input("Enter a number:")
-        if (number == "done"):
+        if number == "done":
             break
         try:
             float(number)
@@ -23,15 +23,15 @@ def Numbers():
 
 
 #3
-def Letter():
+def letter():
     letter = input("Enter the letter: ")
     lst_vowel = ["a","e","i","o","u"]
     lst_consonant = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
-    if(letter == "y"):
+    if letter == "y":
         print("Letter is vowel and consonant")
-    elif(letter in lst_vowel):
+    elif letter in lst_vowel:
         print("Letter is vowel")
-    elif(letter in lst_consonant):
+    elif letter in lst_consonant:
         print("Letter is consonant")
     else:
         print("Wrong input")
@@ -42,9 +42,9 @@ def chess_color():
     letters = ['a','b','c','d','e','f','g','h']
     letter = input("Enter letter coord: ").lower()
     dig = int(input("Enter digital coord: "))
-    if(letter in letters and 0 < dig < 9 ):
+    if letter in letters and 0 < dig < 9:
         index = letters.index(letter)
-        if((index + 1) % 2 == dig % 2):
+        if (index + 1) % 2 == dig % 2:
             print("Color is black")
         else:
             print("Color is white")
